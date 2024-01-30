2320e174b25c361936e21bb71ac4ff0cd36e0693
Shirish Kamath <shirishk.87@gmail.com>
2024-01-30 21:59:39 +0530

    Use node:fs/promises

--- build.mjs
+++ build.mjs
@@ -1,5 +1,5 @@
 import SwaggerParser from '@apidevtools/swagger-parser';
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import path from "node:path";
 import openapiTS, { astToString } from "openapi-typescript";
 import ts from "typescript";
@@ -12,8 +12,8 @@ const dirOutput = './src/generated';
 const main = async () => {
   const api = await SwaggerParser.bundle('./openapi.yml');
   await SwaggerParser.validate(api);
-  await fs.promises.mkdir(dirOutput, { recursive: true });
-  await fs.promises.writeFile(path.join(dirOutput, 'openapi.json'), JSON.stringify(api, null, 2));
+  await fs.mkdir(dirOutput, { recursive: true });
+  await fs.writeFile(path.join(dirOutput, 'openapi.json'), JSON.stringify(api, null, 2));
 
   const ast = await openapiTS(new URL("./openapi.yml", import.meta.url), {
     transform(schemaObject) {
@@ -26,7 +26,7 @@ const main = async () => {
   });
 
   const contents = astToString(ast);
-  fs.promises.writeFile(path.join(dirOutput, 'openapi.ts'), contents);
+  fs.writeFile(path.join(dirOutput, 'openapi.ts'), contents);
 };
 
 main();
