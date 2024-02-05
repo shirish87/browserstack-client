e38f8cef5022622f3f4e3346e21ec090a1d9a45a
Shirish Kamath <shirishk.87@gmail.com>
2024-02-05 22:42:35 +0530

    Use namespace tsdoc, add LocalTestingBinary

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { AutomateClient } from "@/automate.ts"
 import type { components } from "@/generated/openapi.ts"
 import { JSTestingClient } from "@/js-testing.ts"
 import { LocalTestingClient } from "@/local-testing.ts"
+import { LocalTestingBinary } from "@/local-testing-binary.ts"
 import { ScreenshotsClient } from "@/screenshots.ts"
 
 export type { APIFetchOptions, BrowserStackOptions } from "@/api-client.ts"
@@ -11,13 +12,14 @@ export type { AutomateClient } from "@/automate.ts"
 export type { BrowserStackError, ErrorContext } from "@/error.ts"
 export type { JSTestingClient } from "@/js-testing.ts"
 export type { LocalTestingClient } from "@/local-testing.ts"
+export type { LocalTestingBinary } from "@/local-testing-binary.ts"
 export type { ScreenshotsClient } from "@/screenshots.ts"
 
 export type schemas = components["schemas"];
 
 /**
- * Represents the BrowserStack API module.
- * @module
+ * Represents the BrowserStack API namespace.
+ * @namespace
  * @public
  */
 export const BrowserStack = {
@@ -30,4 +32,5 @@ export const BrowserStack = {
    */
   Client: JSTestingClient,
   LocalTestingClient: LocalTestingClient,
+  LocalTestingBinary: LocalTestingBinary,
 };
