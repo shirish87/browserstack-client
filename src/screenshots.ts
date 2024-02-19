d8b0a40f984156ba6caa0bdf6dc876b7dc251d8b
Shirish Kamath <shirishk.87@gmail.com>
2024-02-19 17:30:55 +0530

    Added helper for awaiting screenshots, consistent job.id

--- src/screenshots.ts
+++ src/screenshots.ts
@@ -1,5 +1,10 @@
-import { APIClient, BrowserStackOptions, APIFetchOptions } from "@/api-client.ts"
-import type { operations } from "@/generated/openapi.ts"
+import {
+  APIClient,
+  APIFetchOptions,
+  BrowserStackOptions,
+} from "@/api-client.ts";
+import { BrowserStackError } from "@/error.ts";
+import type { components, operations } from "@/generated/openapi.ts";
 import type { FetchOptions } from "openapi-fetch";
 
 /**
@@ -8,6 +13,11 @@ import type { FetchOptions } from "openapi-fetch";
  * @public
  */
 export class ScreenshotsClient extends APIClient {
+  /**
+   * @internal
+   */
+  private static readonly jobCompleteStates = ["done", "error"];
+
   /**
    * Constructs a new instance of the ScreenshotsClient class.
    * @param options - Optional configuration options for the client.
@@ -41,7 +51,10 @@ export class ScreenshotsClient extends APIClient {
     return this.makePostRequest("/screenshots", {
       ...options,
       body,
-    });
+    }).then((job) => ({
+      ...job,
+      id: job.job_id,
+    }));
   }
 
   /**
@@ -61,6 +74,75 @@ export class ScreenshotsClient extends APIClient {
           jobId,
         },
       },
+    }).then((job) => ({
+      ...job,
+      id: job.id ?? job.job_id,
+    }));
+  }
+
+  /**
+   * Tracks the progress of a screenshot job and retrieves the screenshots associated with it.
+   *
+   * @param jobId - The ID of the job to track.
+   * @param onScreenshot - Optional callback function that will be called when a new screenshot is added to the result.
+   * @param pollInterval - The interval (in milliseconds) at which the job status should be polled. Default is 10000ms.
+   * @returns A promise that resolves with an array of screenshots when the job is completed.
+   */
+  trackJob(
+    jobId: string,
+    onScreenshot?: (
+      screenshot: components["schemas"]["Screenshot"]
+    ) => void | Promise<void>,
+    options?: APIFetchOptions<operations["getScreenshotsJob"]>,
+    pollInterval = 10_000
+  ): Promise<components["schemas"]["Screenshot"][]> {
+    return new Promise((resolve, reject) => {
+      const result: Map<string, components["schemas"]["Screenshot"]> =
+        new Map();
+      let abortController: AbortController | undefined;
+
+      const end = (err?: Error) => {
+        console.error(err);
+        clearInterval(interval);
+        abortController?.abort?.();
+
+        if (err) {
+          reject(
+            err instanceof BrowserStackError
+              ? err
+              : new BrowserStackError(err.message, err)
+          );
+        } else {
+          resolve(Array.from(result.values()));
+        }
+      };
+
+      const interval = setInterval(async () => {
+        abortController = options?.signal ? undefined : new AbortController();
+
+        const job = await this.getJob(jobId, {
+          ...options,
+          signal: options?.signal ?? abortController?.signal,
+        }).catch(end);
+
+        if (!job) return;
+
+        const screenshots = job.screenshots ?? [];
+
+        for (const screenshot of screenshots) {
+          if (ScreenshotsClient.jobCompleteStates.includes(screenshot.state)) {
+            if (!result.has(screenshot.id)) {
+              result.set(screenshot.id, screenshot);
+            }
+
+            await onScreenshot?.(screenshot);
+          }
+        }
+
+        if (screenshots.length === result.size) {
+          end();
+        }
+      }, pollInterval);
     });
   }
 }
