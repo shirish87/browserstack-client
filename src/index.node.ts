61f4bcfcc7ad92df387c3f1e76f871faa6cfdd2a
Shirish Kamath <shirishk.87@gmail.com>
2024-02-20 12:57:15 +0530

    Tree-shaking friendly exports

--- src/index.node.ts
+++ src/index.node.ts
@@ -2,11 +2,12 @@ import { BrowserStack as B } from "@/index.ts";
 import { LocalTestingBinary } from "@/local-testing-binary.ts";
 
 export * from "@/index.ts";
+export { LocalTestingBinary } from "@/local-testing-binary.ts";
+
 export type {
   LocalBinaryFlags,
   LocalBinaryFolderTestingFlags,
   LocalBinaryServerTestingFlags,
-  LocalTestingBinary,
   LocalTestingBinaryOptions,
   ProxyParams,
 } from "@/local-testing-binary.ts";
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ import { LocalTestingClient } from "@/local-testing.ts";
 import { ScreenshotsClient } from "@/screenshots.ts";
 
 export type { APIFetchOptions, BrowserStackOptions } from "@/api-client.ts";
-export type { AppAutomateClient, FlutterPlatform } from "@/app-automate.ts";
-export type { AutomateClient } from "@/automate.ts";
+export { AppAutomateClient, FlutterPlatform } from "@/app-automate.ts";
+export { AutomateClient } from "@/automate.ts";
 export type { BrowserStackError, ErrorContext } from "@/error.ts";
-export type { JSTestingClient } from "@/js-testing.ts";
-export type { LocalTestingClient, LocalTestingOptions } from "@/local-testing.ts";
-export type { ScreenshotsClient } from "@/screenshots.ts";
+export { JSTestingClient } from "@/js-testing.ts";
+export { LocalTestingClient } from "@/local-testing.ts";
+export type { LocalTestingOptions } from "@/local-testing.ts";
+export { ScreenshotsClient } from "@/screenshots.ts";
 
 /**
  * Represents the BrowserStack API namespace.
