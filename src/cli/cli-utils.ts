4d08ee9fa800ee335277ae7e5c55444ceb7155d0
Shirish Kamath <shirishk.87@gmail.com>
2024-03-04 11:59:03 +0530

    Fix missing username error message

--- src/cli/cli-utils.ts
+++ src/cli/cli-utils.ts
@@ -22,18 +22,18 @@ export function ensureKeyExists(key: string | undefined): string {
 
 /**
  * Ensures that a username exists and returns it.
- * If the key is undefined, it falls back to the value of env.BROWSERSTACK_USERNAME.
+ * If the username is undefined, it falls back to the value of env.BROWSERSTACK_USERNAME.
  * Throws a BrowserStackError if the key is missing or empty.
- * @param key - The key to ensure.
- * @returns The ensured key.
- * @throws {BrowserStackError} If the key is missing or empty.
+ * @param username - The username to ensure.
+ * @returns The ensured username.
+ * @throws {BrowserStackError} If the username is missing or empty.
  *
  * @internal
  */
-export function ensureUsernameExists(key: string | undefined): string {
-  const username = key ?? env.BROWSERSTACK_USERNAME;
+export function ensureUsernameExists(data: string | undefined): string {
+  const username = data ?? env.BROWSERSTACK_USERNAME;
   if (typeof username !== "string" || !username.trim().length) {
-    throw new BrowserStackError("Missing key");
+    throw new BrowserStackError("Missing username");
   }
 
   return username.trim();
